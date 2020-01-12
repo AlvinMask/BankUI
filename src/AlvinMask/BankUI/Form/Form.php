92132d7b59f4b4465ff79229ca819b9e8854a4b4
AlvinMask <59598775+AlvinMask@users.noreply.github.com>
2020-01-12 15:21:34 +0700

    Create Form.php

---
+++ src/AlvinMask/BankUI/Form/Form.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AlvinMask\BankUI\Form;
+
+use pocketmine\form\Form as IForm;
+use pocketmine\Player;
+
+abstract class Form implements IForm{
+
+    protected $data = [];
+    private $callable;
+
+    public function __construct(?callable $callable){
+     $this->callable = $callable;
+    }
+
+    public function getCallable():?callable{
+     return $this->callable;
+    }
+
+    public function setCallable(?callable $callable){
+     $this->callable = $callable;
+    }
+
+    public function handleResponse(Player $p, $data):void{
+     $this->processData($data);
+     $callable = $this->getCallable();
+     if($callable !== null){
+      $callable($p, $data);
+     }
+    }
+
+    public function processData(&$data):void{
+    }
+
+    public function jsonSerialize(){
+     return $this->data;
+    }
+
+}
